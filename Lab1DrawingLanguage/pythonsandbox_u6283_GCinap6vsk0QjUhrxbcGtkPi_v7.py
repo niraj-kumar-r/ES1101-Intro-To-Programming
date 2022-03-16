27cd555d83ef32a35759548b53b536b2492c255c
niraj-kumar-r <nirajk23523@gmail.com>
2022-03-16 21:41:47 +0530

    fig 5 done

---
+++ Lab1DrawingLanguage/pythonsandbox_u6283_GCinap6vsk0QjUhrxbcGtkPi_v7.py
@@ -0,0 +1,15 @@
+import turtle
+import math
+t = turtle.Turtle()
+t.speed(10) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+scale=100
+numOfCir=36
+
+t.pendown()
+
+for i in range(36):
+  t.circle(scale)
+  t.right(360/numOfCir)
+
+  
+turtle.done()
