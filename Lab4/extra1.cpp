654829c678666d0814f11e07867406db8e0756e2
niraj-kumar-r <nirajk23523@gmail.com>
2022-04-08 18:04:12 +0530

    extra1

--- Lab4/extra1.cpp
+++ Lab4/extra1.cpp
@@ -3,26 +3,37 @@
 
 using namespace std;
 
-float calcLog10(float num, float divFactor = 10);
+double calcLog10(double num, int precisionFactor = 10);
 
 int main(void)
 {
-    float n = 1;
+    double n = 1;
     cout << "Enter a number : ";
     cin >> n;
-    cout << calcLog10(n);
+    double log10OfN = calcLog10(n);
+    cout << "Our function gives " << log10OfN << "\n";
+    cout << "Actual value " << log10(n) << "\n";
     return 0;
 }
 
-float calcLog10(float num, float divFactor)
+double calcLog10(double num, int precisionFactor)
 {
+    double log10Value = 0;
+    double naturalLogValue = 0;
+    double eValue = exp(1);
 
-    if (num < divFactor)
+    while ((num / eValue) > 1)
     {
-        return num;
+        naturalLogValue += 1;
+        num /= eValue;
     }
-    else
+
+    for (int i = 1; i <= precisionFactor; i++)
     {
-        return calcLog10(num / divFactor);
+        naturalLogValue += (pow(-1, i - 1) * pow(num - 1, i)) / (i);
     }
+
+    log10Value += naturalLogValue / 2.303;
+
+    return log10Value;
 }
