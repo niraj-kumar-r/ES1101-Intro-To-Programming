0ae5ff7eaf8c6531c5034173cb659283d573a28d
niraj-kumar-r <nirajk23523@gmail.com>
2022-04-08 15:43:39 +0530

    extra 1 started

---
+++ Lab4/extra1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+float calcLog10(float num);
+
+int main(void)
+{
+    float n = 1;
+    cout << "Enter a number : ";
+    cin >> n;
+    cout << calcLog10(n);
+    return 0;
+}
+
+float calcLog10(float num)
+{
+}
