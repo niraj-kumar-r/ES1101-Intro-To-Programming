3eb5fe6d919a39a68e6a1bc3c42421d7be69f5e5
niraj-kumar-r <nirajk23523@gmail.com>
2022-04-17 15:04:19 +0530

    extra 1 done

--- Lab5/extra1.cpp
+++ Lab5/extra1.cpp
@@ -8,19 +8,23 @@ using namespace std;
 int main(void)
 {
     int n = 3;
+    // defaukt length of array A
+
+    int elemets_to_sum = 3;
+
     while (true)
     {
         cout << "Enter the number of elements in the array: ";
         cin >> n;
 
-        if (n >= 3)
+        if (n >= elemets_to_sum)
         {
             break;
         }
 
         else
         {
-            cout << "INVALID INPUT. The number of elements must be more than or equal to 3. Try again\n";
+            cout << "INVALID INPUT. The number of elements must be more than or equal to " << elemets_to_sum << ". Try again\n";
         }
     }
 
@@ -32,4 +36,33 @@ int main(void)
     {
         cin >> A[i];
     }
+
+    int min_sum_array[elemets_to_sum] = {};
+
+    for (int i = 0; i < elemets_to_sum; i++)
+    {
+        int min_element = A[i];
+        int min_index = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (A[j] < min_element)
+            {
+                min_element = A[j];
+                min_index = j;
+            }
+        }
+
+        A[min_index] = A[i];
+        A[i] = min_element;
+        min_sum_array[i] = min_element;
+    }
+
+    cout << "The triplet with min sum is: ";
+
+    for (int k = 0; k < elemets_to_sum; k++)
+    {
+        cout << min_sum_array[k] << " ";
+    }
+    cout << "\n";
 }
