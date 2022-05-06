58653ae374073f32ba1f8a2f3ba8d83fcb4eeb02
niraj-kumar-r <nirajk23523@gmail.com>
2022-05-06 08:44:14 +0530

    ques2 done

--- Lab7/ques2.cpp
+++ Lab7/ques2.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <string.h>
+#include <cstring>
 using namespace std;
 
 int countOccurrences(char Sentence[], char word[]);
@@ -11,47 +11,49 @@ int countOccurrences(char Sentence[], char word[]);
 int main()
 {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    return 0;
-}
 
-int countOccurrences(char Sentence[], char word[])
-{
-    int t;
+    int t = 1;
     cin >> t;
     cin.ignore();
 
     while (t--)
     {
-        char sentence[1001];
-        cin.getline(sentence, 1001);
+        char sen[1001], w[100];
+        cin.getline(sen, 1001);
+        cin.getline(w, 100);
 
-        char word[100];
-        cin.getline(word, 100);
+        cout << countOccurrences(sen, w) << '\n';
+    }
+    return 0;
+}
 
-        char n_word[100];
-        int n_len = 0;
+int countOccurrences(char sentence[], char word[])
+{
 
-        int count = 0;
+    char n_word[100];
+    int n_len = 0;
 
-        for (int i = 0; sentence[i] != '\0'; i++)
+    int count = 0;
+
+    for (int i = 0; sentence[i] != '\0'; i++)
+    {
+        if (sentence[i] == ' ' || sentence[i] == '.' || sentence[i] == ',')
         {
-            if (sentence[i] == '.' || sentence[i] == ',')
+            n_word[n_len] = '\0';
+
+            if (strcmp(n_word, word) == 0)
             {
-                n_word[n_len] = '\0';
-
-                if (strcmp(n_word, word) == 0)
-                {
-                    count++;
-                }
-
-                n_len = 0;
+                count++;
             }
 
-            else
-            {
-                n_word[n_len] = sentence[i];
-                n_len++;
-            }
+            n_len = 0;
+        }
+
+        else
+        {
+            n_word[n_len] = sentence[i];
+            n_len++;
         }
     }
+    return count;
 }
